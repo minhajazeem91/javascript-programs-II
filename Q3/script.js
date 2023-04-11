55c80c393c5faa18d792431fdf0b9d4c607b6875
Minhaj <minhajazeem91@gmail.com>
2023-04-12 04:12:41 +0500

    function that adds a new paragraph element

---
+++ Q3/script.js
@@ -0,0 +1,6 @@
+//  function that adds a new paragraph element
+
+function addParagraph(para){
+    document.write('<p>'+para+'</p>');
+}
+addParagraph("This is a 3rd question of the 2nd assignment.");
